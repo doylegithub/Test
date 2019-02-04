79beb4ddf8868c7fff446394f52cd35f89bd1fb8
doyle <doyle@localhost.localdomain>
2019-02-04 12:38:14 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 
 class App extends Component {
   render() {
     return (
-      <View style={Styles.container}>
-          <Text style={}>hellow world</Text>
+      <View style={styles.container}>
+          <Text style={styles.text}>hellow world</Text>
 
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 500 ,
+    backgroundColor: 'purple',
+  },
+
+  text: {
+    fontSize: 32,
+    alignSelf: 'center',
+    color: 'white',
+  }
+
+
+})
 export default App;
